a114fac0626ed43ba013b8ff516f95f44e0d5428
okhere21@gmail.com <om@oslab-zetalinux.is.localnet>
2026-08-05 14:32:06 +0200

    Freeze the VIT Backbone

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ scripts/train.sh --name horse_v1 -- \
     DATASETS.HORSE.ROOT_IMAGE=/lustre/home/okumar/outputs/batches \
     DATASETS.HORSE.JSON_FILE.TRAIN=/lustre/home/okumar/outputs/horse_dataset/train_subset_50.json \
     DATASETS.HORSE.JSON_FILE.TEST=/lustre/home/okumar/outputs/horse_dataset/test_subset_10.json \
-    DATASETS.ANIMAL3D.WEIGHT=0 DATASETS.CUB.WEIGHT=0 DATASETS.CTRLAVES3D.WEIGHT=0
+    DATASETS.ANIMAL3D.WEIGHT=0 DATASETS.CUB.WEIGHT=0 DATASETS.CTRLAVES3D.WEIGHT=0 \
+    MODEL.BACKBONE.FREEZE_ATTN=true MODEL.BACKBONE.FREEZE_FFN=true
