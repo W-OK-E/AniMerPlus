5cbee7946eb509cc9b26a6a1d6b5725acc8bc302
Om Kumar <okumar@login2.is.localnet>
2026-08-14 06:02:08 +0200

    Changes from the cluster

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-scripts/train.sh --name horse_v1 -- \
+scripts/train.sh --name horse_orientation-fixed -- \
     DATASETS.HORSE.WEIGHT=1 \
     DATASETS.HORSE.ROOT_IMAGE=/lustre/home/okumar/outputs/batches \
-    DATASETS.HORSE.JSON_FILE.TRAIN=/lustre/home/okumar/outputs/horse_dataset/train_subset_50.json \
-    DATASETS.HORSE.JSON_FILE.TEST=/lustre/home/okumar/outputs/horse_dataset/test_subset_10.json \
+    DATASETS.HORSE.JSON_FILE.TRAIN=/lustre/home/okumar/outputs/horse_dataset/train.json \
+    DATASETS.HORSE.JSON_FILE.TEST=/lustre/home/okumar/outputs/horse_dataset/test.json \
     DATASETS.ANIMAL3D.WEIGHT=0 DATASETS.CUB.WEIGHT=0 DATASETS.CTRLAVES3D.WEIGHT=0 \
     MODEL.BACKBONE.PRETRAINED_WEIGHTS=data/AniMerPlus/checkpoint.ckpt \
     MODEL.BACKBONE.FREEZE_ATTN=true MODEL.BACKBONE.FREEZE_FFN=true \
