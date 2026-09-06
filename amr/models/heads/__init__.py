13637a3fbb3b8e385260b76425280fb0bf427688
okhere21@gmail.com <om@oslab-zetalinux.is.localnet>
2026-09-07 00:02:46 +0200

    More cleanup

--- amr/models/heads/__init__.py
+++ amr/models/heads/__init__.py
@@ -1,11 +1,5 @@
-from .aves_head import build_aves_head
-from .classifier_head import Classifer
-from .smal_head import build_smal_head
 from .varen_head import build_varen_head
 
 __all__ = [
-    "Classifer",
-    "build_aves_head",
-    "build_smal_head",
     "build_varen_head"
 ]
